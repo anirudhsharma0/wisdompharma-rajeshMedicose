e8b01481a3cad01d78fe10995e80a854a6899034
royaltraditionalcraft <royaltraditionalcraft@gmail.com>
2026-08-13 17:30:55 +0530

    Fix import conflict for FirebaseService in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart' hide FirebaseService;
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'firebase_options.dart';
 import 'core/theme/app_theme.dart';
